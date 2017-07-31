b6ba5b02f31f8a109927af0b732c3570c3132e7d
Joseph Kim <joseph.kim.kr@gmail.com>
2017-07-31 15:47:20 -0700

    fixed off by 1 bug for computing ending square coordinate

--- game.js
+++ game.js
@@ -38,7 +38,36 @@ function generateHorizontalShipLocations(openSpaces, shipSpaces, horizontalShips
 
     // since it is from the openSpaces array, the starting square will always be valid
     var startingSquare = openSpaces[randomNumber(0, openSpaces.length)];
-    var endingSquare = startingSquare + horizontalShips[i];
+    var endingSquare = startingSquare + (horizontalShips[i] - 1);
+
+
+    // checks which row the startingSquare and endingSquare are on
+    var startingSquareRow = Math.floor(startingSquare / 10);
+    var endingSquareRow = Math.floor(endingSquare / 10);
+
+    // Fixs the position of the ship
+    //    if startingSquare and endingSquare are not on the same row, then have to pick another square
+    while (endingSquareRow - startingSquareRow !== 0 || shipSpaces.includes(startingSquareRow) || shipSpaces.includes(endingSquareRow)) {
+      startingSquare = openSpaces[randomNumber(0, openSpaces.length)];
+      endingSquare = startingSquare + horizontalShips[i];
+    }
+
+    // Adds the ship's coordinates to the shipSpaces array and removes them from the open spaces array
+    for (var j = 0; j < horizontalShips[i]; j++) {
+      var thisShipCoordinate = startingSquare + j;
+      shipSpaces.push(thisShipCoordinate);
+      openSpaces.splice(openSpaces.indexOf(thisShipCoordinate), 1);
+    }
+  }
+}
+
+function generateVeriticalShipLocations(openSpaces, shipSpaces, verticalShips) {
+
+  for (var i = 0; i < verticalShips.length; i++) {
+
+    // since it is from the openSpaces array, the starting square will always be valid
+    var startingSquare = openSpaces[randomNumber(0, openSpaces.length)];
+    var endingSquare = startingSquare + verticalShips[i];
 
 
     // checks which row the startingSquare and endingSquare are on

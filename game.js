05c099f09627798d30de4bb8980b66bb5f3e7009
Gabe Altenberger <gabealtenberger@gmail.com>
2017-07-31 11:15:55 -0700

    added global arrays

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
 'use strict';
+
+var openSpaces = [];
+var shipSpaces = [];
+var hits = [];
+var misses = [];
+
+for (var i = 1; i <= 100; i++) {
+  openSpaces.push(i);
+}
+
+function spaceRows() {
+  for (var i = 0; i < openSpaces.length; i++) {
+
+  }
+}

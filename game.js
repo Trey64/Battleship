f7015ed431b7f423cda742ab792013dc79194ccd
Joseph Kim <joseph.kim.kr@gmail.com>
2017-07-31 14:43:52 -0700

    added method to check if a horizontal ship fits on one row

--- game.js
+++ game.js
@@ -18,7 +18,7 @@ var bottomVerticalShips = [];
 var bottomShipLengths = [2, 3, 3, 4, 5];
 
 
-function determineShipOrientation(shipArray, verticalShips, horizontalShips) {
+function determineShipOrientation(shipArray, horizontalShips, verticalShips) {
   for (var i = 0; i < shipArray.length; i++){
     var randomNum = rand(0, 1);
     if (randomNum === 0){
@@ -29,8 +29,40 @@ function determineShipOrientation(shipArray, verticalShips, horizontalShips) {
   }
 }
 
-function generateShipLocations() {
-  
+// Randomly generates the positions of the horizontal ships
+// openSpaces will be the open spaces available to place a shipSpaces
+// shipSpaces will be the squares that contain a ship
+// horinzotalShips will contain the lengths of the horizontal ships (that we will place)
+function generateHorizontalShipLocations(openSpaces, shipSpaces, horizontalShips) {
+
+  for (var i = 0; i < horizontalShips.length; i++) {
+
+    // since it is from the openSpaces array, the starting square will always be valid
+    var startingSquare = openSpace[rand(0, openSpace.length)]; // a random square from the openSpaces array
+    var endingSquare = startingSquare + horizontalShips[i];
+
+
+    // checks which row the startingSquare and endingSquare are on
+    var startingSquareRow = Math.floor(startingSquare / 10);
+    var endingSquareRow = Math.floor(endingSquare / 10);
+
+    // Fix the position of the ship, if the position
+    // if startingSquare and endingSquare are not on the same row, then have to pick another square
+    while (endingSquareRow - startingSquareRow !== 0) {
+      startingSquare = openSpace[rand(0, openSpace.length)];
+      endingSquare = startingSquare + horizontalShips[i];
+    }
+
+    var shipCoordinates = [];
+
+    // make an array of the ship coordinates
+    //      for example for a ship of length 3 starting at 48: [48, 49, 50]
+    for (var j = 0; j < horizontalShips[i]; j++) {
+      shipCoordinates[j] = (startingSquare + j);
+    }
+
+  }
+
 }
 
 function rand (min, max) {

5f7ea3b8c15bbc0cd41705e9fa41328dfb117cb0
Gabe Altenberger <gabealtenberger@gmail.com>
2017-07-31 11:46:23 -0700

    added some math

--- game.js
+++ game.js
@@ -8,7 +8,6 @@ var topMisses = [];
 var topHorizontalShips = [];
 var topVerticalShips = [];
 var topShipLengths = [2, 3, 3, 4, 5];
-
 //Bottom Board
 var bottomOpenSpaces = [];
 var bottomShipSpaces = [];
@@ -19,8 +18,22 @@ var bottomVerticalShips = [];
 var bottomShipLengths = [2, 3, 3, 4, 5];
 
 
+function determineShipOrientation(shipArray, verticalShips, horizontalShips) {
+  for (var i = 0; i < shipArray.length; i++){
+    var randomNum = rand(0, 1);
+    if (randomNum === 0){
+      verticalShips.push(shipArray[i]);
+    } else {
+      horizontalShips.push(shipArray[i]);
+    }
+  }
+}
 
-
+function rand (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 for (var i = 1; i <= 100; i++) {

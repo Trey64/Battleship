b123f2d3b3e1cc92073c3104386e1fbae2c2912b
Joseph Kim <joseph.kim.kr@gmail.com>
2017-08-01 01:00:54 -0700

    refactored entire game.js to use object constructors

--- game.js
+++ game.js
@@ -13,10 +13,18 @@ function Battleship(ships) {
   this.ships = ships;
 }
 
+// method to generate an empty game board with numbers from 0 to 99
+//   each number represents a square on a classic 10 x 10 board
+Battleship.prototype.populateOpenSquares = function() {
+  for (var i = 0; i <= 99; i++) {
+    this.openSquares.push(i);
+  }
+};
+
 // method for each battleship object to randomly determine which ships are vertical
 //   and which ships are horizontal
 Battleship.prototype.determineShipOrientation = function() {
-  for (var i = 0; i < this.shipSizes; i++) {
+  for (var i = 0; i < this.ships.length; i++) {
     if (randomNumber(0, 1) === 0) {
       this.horizontalShips.push(this.ships[i]);
     } else {
@@ -104,16 +112,18 @@ Battleship.prototype.generateVerticalShipLocations = function() {
   }
 };
 
-var topBoard = new Battleship(ships);
-var bottomBoard = new Battleship(ships);
+// method to show the ship positions on the game board
+Battleship.prototype.renderShipPositions = function() {
+  for (var i = 0; i < this.shipSquares.length; i++) {
 
-topBoard.determineShipOrientation();
-topBoard.generateHorizontalShipLocations();
-topBoard.generateVerticalShipLocations();
+    // get the td element id
+    var tdIdentifier = 'b' + this.shipSquares[i];
 
-bottomBoard.determineShipOrientation();
-bottomBoard.generateHorizontalShipLocations();
-bottomBoard.generateVerticalShipLocations();
+    // give the corresponding squares color
+    var tdEl = document.getElementById(tdIdentifier);
+    tdEl.style.backgroundColor = 'gray';
+  }
+};
 
 // returns an integer between the passed min and max parameters (both ends inclusve)
 function randomNumber(min, max) {
@@ -122,150 +132,20 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function determineShipOrientation(shipArray, horizontalShips, verticalShips) {
-  for (var i = 0; i < shipArray.length; i++){
-    if (randomNumber(0, 1) === 0){
-      verticalShips.push(shipArray[i]);
-    } else {
-      horizontalShips.push(shipArray[i]);
-    }
-  }
-}
+////////////////////////////////////////////////////////////////////////////////
+//////////          Method Calls           /////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-//Top Board
-var topOpenSpaces = [];
-var topShipSpaces = [];
-var topHits = [];
-var topMisses = [];
-var topHorizontalShips = [];
-var topVerticalShips = [];
-var topShipLengths = [2, 3, 3, 4, 5];
-//Bottom Board
-var bottomOpenSpaces = [];
-var bottomShipSpaces = [];
-var bottomHits = [];
-var bottomMisses = [];
-var bottomHorizontalShips = [];
-var bottomVerticalShips = [];
-var bottomShipLengths = [2, 3, 3, 4, 5];
+var topBoard = new Battleship(ships);
+var bottomBoard = new Battleship(ships);
 
+topBoard.populateOpenSquares();
+topBoard.determineShipOrientation();
+topBoard.generateHorizontalShipLocations();
+topBoard.generateVerticalShipLocations();
 
-
-// Randomly generates the positions of the horizontal ships
-// openSpaces will be the open spaces available to place a shipSpaces
-// shipSpaces will be the squares that contain a ship
-// horinzotalShips will contain the lengths of the horizontal ships (that we will place)
-function generateHorizontalShipLocations(openSpaces, shipSpaces, horizontalShips) {
-
-  for (var i = 0; i < horizontalShips.length; i++) {
-
-    // since it is from the openSpaces array, the starting square will always be valid
-    var startingSquare = openSpaces[randomNumber(0, openSpaces.length)];
-    var endingSquare = startingSquare + (horizontalShips[i] - 1);
-
-    // checks which row the startingSquare and endingSquare are on
-    var startingSquareRow = Math.floor(startingSquare / 10);
-    var endingSquareRow = Math.floor(endingSquare / 10);
-
-    // gets the index of the starting and ending squares in the open spaces array
-    var indexOfStartingSquare = openSpaces.indexOf(startingSquare);
-    var indexOfEndingSquare = openSpaces.indexOf(endingSquare);
-
-    // Fixes the position of the ship
-    while (endingSquareRow - startingSquareRow !== 0 || shipSpaces.includes(startingSquareRow) || shipSpaces.includes(endingSquareRow) || indexOfEndingSquare - indexOfStartingSquare !== (horizontalShips[i] - 1) || typeof startingSquare !== 'number') {
-      startingSquare = openSpaces[randomNumber(0, openSpaces.length)];
-      endingSquare = startingSquare + (horizontalShips[i] - 1);
-      startingSquareRow = Math.floor(startingSquare / 10);
-      endingSquareRow = Math.floor(endingSquare / 10);
-      indexOfStartingSquare = openSpaces.indexOf(startingSquare);
-      indexOfEndingSquare = openSpaces.indexOf(endingSquare);
-    }
-
-    // Adds the ship's coordinates to the shipSpaces array and removes them from the open spaces array
-    for (var j = 0; j < horizontalShips[i]; j++) {
-      var tempShipCoordinate = startingSquare + j;
-      shipSpaces.push(tempShipCoordinate);
-      openSpaces.splice(openSpaces.indexOf(tempShipCoordinate), 1);
-    }
-  }
-}
-
-// Randomly generates the positions of the vertical ships
-// openSpaces will be the open spaces available to place a shipSpaces
-// shipSpaces will be the squares that contain a ship
-// verticalShips will contain the lengths of the vertical ships (that we will place)
-function generateVerticalShipLocations(openSpaces, shipSpaces, verticalShips) {
-
-  for (var i = 0; i < verticalShips.length; i++) {
-
-    // since it is from the openSpaces array, the starting square will always be valid
-    var startingVerticalSquare = openSpaces[randomNumber(0, openSpaces.length)];
-
-    while(startingVerticalSquare >= 80) {
-      startingVerticalSquare = openSpaces[randomNumber(0, openSpaces.length)];
-    }
-
-    var endingVerticalSquare = startingVerticalSquare + ((verticalShips[i] - 1) * 10);
-
-    var thisShipCoordinates = [];
-
-    for (var j = 0; j < verticalShips[i]; j++) {
-      thisShipCoordinates[j] = startingVerticalSquare + (10 * j);
-    }
-
-    // helper function to check if any of this vertical ship coordinates are already taken
-    var checkIfContains = function(haystack, arr) {
-      return arr.some(function (v) {
-        return haystack.indexOf(v) >= 0;
-      });
-    };
-
-    while (endingVerticalSquare >= 99 || checkIfContains(shipSpaces, thisShipCoordinates) || typeof startingVerticalSquare !== 'number') {
-      startingVerticalSquare = openSpaces[randomNumber(0, openSpaces.length)];
-      endingVerticalSquare = startingVerticalSquare + ((verticalShips[i] - 1) * 10);
-      for (var k = 0; k < verticalShips[i]; k++) {
-        thisShipCoordinates[k] = startingVerticalSquare + (10 * k);
-      }
-    }
-
-    // Adds the ship's coordinates to the shipSpaces array and removes them from the open spaces array
-    for (var p = 0; p < verticalShips[i]; p++) {
-      var tempShipCoordinate = startingVerticalSquare + (p * 10);
-      shipSpaces.push(tempShipCoordinate);
-      openSpaces.splice(openSpaces.indexOf(tempShipCoordinate), 1);
-    }
-  }
-}
-
-
-function createOpenSpaces() {
-  for (var i = 0; i <= 99; i++) {
-    topOpenSpaces.push(i);
-    bottomOpenSpaces.push(i);
-  }
-}
-
-function renderBottomShips() {
-  for (var i = 0; i < bottomShipSpaces.length; i++) {
-
-    // get the td element id
-    var tdIdentifier = 'b' + bottomShipSpaces[i];
-
-    // give the corresponding squares color
-    var tdEl = document.getElementById(tdIdentifier);
-    tdEl.style.backgroundColor = 'gray';
-  }
-}
-
-createOpenSpaces();
-
-determineShipOrientation(topShipLengths, topHorizontalShips, topVerticalShips);
-determineShipOrientation(bottomShipLengths, bottomHorizontalShips, bottomVerticalShips);
-
-generateVerticalShipLocations(topOpenSpaces, topShipSpaces, topVerticalShips);
-generateHorizontalShipLocations(topOpenSpaces, topShipSpaces, topHorizontalShips);
-
-generateVerticalShipLocations(bottomOpenSpaces, bottomShipSpaces, bottomVerticalShips);
-generateHorizontalShipLocations(bottomOpenSpaces, bottomShipSpaces, bottomHorizontalShips);
-
-renderBottomShips();
+bottomBoard.populateOpenSquares();
+bottomBoard.determineShipOrientation();
+bottomBoard.generateHorizontalShipLocations();
+bottomBoard.generateVerticalShipLocations();
+bottomBoard.renderShipPositions();
